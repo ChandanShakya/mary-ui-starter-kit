ec33aaf9a750ca3ee5c060373f55867c68040b93
ChandanShakya <email@chandanshakya.com.np>
2026-02-24 01:49:06 +0545

    Fixes

--- api/index.php
+++ api/index.php
@@ -1,15 +1,10 @@
 <?php
-
-$directories = [
+foreach ([
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache',
     '/tmp/storage/framework/sessions',
+    '/tmp/livewire-tmp',
     '/tmp/storage/logs',
-];
-
-foreach ($directories as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
-    }
-}
+] as $dir) {
+    if (!is_dir($dir)) mkdir($dir, 0755, true);
 require __DIR__.'/../public/index.php';
