0d27ced51f66f98872a02de1ce0a4dc5497bddf6
ChandanShakya <email@chandanshakya.com.np>
2026-02-24 01:29:52 +0545

    Vercel path

--- api/index.php
+++ api/index.php
@@ -1,15 +1,2 @@
 <?php
-// Create required tmp directories before Laravel boots
-$directories = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/logs',
-];
-
-foreach ($directories as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
-    }
-}
 require __DIR__.'/../public/index.php';
